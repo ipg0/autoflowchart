0c15ecddf726ff9954d3333a39413223eb6148ff
notblank00 <pyromancy00@gmail.com>
2020-11-10 23:42:46 +0300

    добавил еще одно правило к санитайзеру

--- sanitizer.js
+++ sanitizer.js
@@ -6,6 +6,8 @@ module.exports = {
             if(tokens[i].type == 'name' && tokens[i - 1].type == 'name' && tokens[i].value == ';' && (tokens[i - 1].value == 'readln' || tokens[i - 1].value == 'read' || tokens[i - 1].value == 'writeln')) {
                 tokens.splice(i, 0, new token('paren', '('), new token('paren', ')'));
             }
+            if(tokens[i].type == 'name' && tokens[i].value == 'end' && (tokens[i - 1].type != 'name' || tokens[i - 1].value != ';'))
+                tokens.splice(i, 0, new token('name', ';'));
         }
         return tokens;
     }
